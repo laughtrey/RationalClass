e6c77c4c08a46a007edc10e5357acb901d497c3e
Phillip Tran <philliptran@cis.vvc.edu>
2018-11-07 16:45:00 -0800

    Function Updates

--- main.cpp
+++ main.cpp
@@ -9,25 +9,56 @@
 
 class Rational
 {
-private:
+	public:
+		Rational(); //Do we need this?
+		Rational(double x, double y); //If we have this?
+		double Adding(const double& m_x, const double& m_y);
+		double Subtraction(const double& m_x, const double& m_y);
+		double Division(const double& m_x, const double& m_y);
+		double Multiplication(const double& m_x, double& m_y);
+		void Print();
 
-double m_x,m_y;
-
-public:
-Rational(); //Do we need this?
-Rational(double x, double y); //If we have this?
-double Adding(const double& m_x, const double& m_y);
-double Subtraction(const double& m_x, const double& m_y);
-double Division(const double& m_x, const double& m_y);
-double Multiplication(const double& m_x, double& m_y);
-std::string Print();
+	private:
+		double m_x, m_y;
 };
+
+Rational::Rational()
+{
+	m_x = 0.0;
+	m_y = 0.0;
+}
 
 Rational::Rational(double x, double y)
 {
-   m_x = x;
-   m_y = y;
+   	m_x = x;
+   	m_y = y;
 }
+
+double Rational::Adding(const double& m_x, const double& m_y)
+{
+	return (m_x + m_y);
+}
+
+double Rational::Subtraction(const double& m_x, const double& m_y)
+{
+	return (m_x - m_y);	
+}
+
+double Rational::Division(const double& m_x, const double& m_y)
+{
+	return (m_x / m_y);
+}
+
+double Rational::Multiplication(const double& m_x, double& m_y)
+{
+	return (m_x * m_y);
+}
+
+void Rational::Print()
+{
+	std::cout << m_x << "/" << m_y;
+}
+
 int main()
 {
 
