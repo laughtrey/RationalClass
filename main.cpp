7bc9472cb9bb9efc33c46051813d0c5a050cf560
JoshuaSantillan <37349382+JoshuaSantillan@users.noreply.github.com>
2018-11-07 17:06:26 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
    File: main.cpp
-   Description:
+   Description: Class Object that solves rational equations through multiple functions and prints using a void function
    Author(s): Laughrey, Raymond. Santillan, Joshua. Tran, Phillip.
    Email: raymondl4963@student.vvc.edu
-   Date of Creation:
+   Date of Creation: 11/7/18
  */
 #include <iostream>
 
@@ -12,11 +12,11 @@ class Rational
 	public:
 		Rational(); //Do we need this?
 		Rational(double x, double y); //If we have this?
-		double Adding(const double& m_x, const double& m_y);
-		double Subtraction(const double& m_x, const double& m_y);
-		double Division(const double& m_x, const double& m_y);
-		double Multiplication(const double& m_x, double& m_y);
-		void Print();
+		double Adding(const double& m_x, const double& m_y); // addition
+		double Subtraction(const double& m_x, const double& m_y); // subtraction
+		double Division(const double& m_x, const double& m_y); // division
+		double Multiplication(const double& m_x, double& m_y); // multiplication
+		void Print(); // void function to print to screen
 
 	private:
 		double m_x, m_y;
@@ -54,7 +54,7 @@ double Rational::Multiplication(const double& m_x, double& m_y)
 	return (m_x * m_y);
 }
 
-void Rational::Print() // prints the lines
+void Rational::Print() // prints the math operations using arithmetic signs
 {
 	std::cout << m_x << " + " << m_y << " = " << Adding(m_x, m_y) << "\n";
 	std::cout << m_x << " - " << m_y << " = " << Subtraction(m_x, m_y) << "\n";
@@ -64,7 +64,7 @@ void Rational::Print() // prints the lines
 
 int main()
 {
-	Rational alpha(5.0, -3.0);
-	alpha.Print();
+	Rational alpha(5.0, -3.0); // creates an object in rational called alpha hardcoding 5 and -3 to get back rational #s
+	alpha.Print(); // prints out the expressions
        return 0;
 }
